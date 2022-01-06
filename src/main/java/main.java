ebe638ee3011960fab14e4a4b3f64297bdb20b0f
John <john3210of@gmail.com>
2022-01-06 19:34:22 +0900

    D+12_mogakco

---
+++ src/main/java/main.java
@@ -0,0 +1,63 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+        }
+    }
+}
+//        System.out.println("hello 월드");
+//
+//        int a = 10;
+//        float b = 0.12345f;
+//        double c = 111111111;
+//        char chr = 'a';
+//        String str = "javaprac";
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(chr);
+//        System.out.println(str);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("======for문입니다======");
+//        System.out.print("숫자를 입력하세요: ");
+//        int a = sc.nextInt();
+//        int temp = 0;
+//        //for문
+//        for(int b=0;b<=a;b++){
+//            temp += b;
+//        }
+//        int sum =temp;
+//        System.out.printf("1부터 %d까지의 합은 %d입니다.\n\n\n",a,sum);
+//
+//        System.out.println("======while문입니다======");
+//        System.out.print("숫자를 입력하세요: ");
+//        int i = sc.nextInt();
+//        int j = 0;
+//        temp=0;
+//
+//        while(j<i){
+//            j+=1;
+//            temp +=j;
+//        }
+//        sum = temp;
+//        System.out.printf("1부터 %d까지의 합은 %d입니다.",i,sum);
+//        sc.close();
+
+//        int length = 5;
+//        System.out.print("숫자 5개를 띄어쓰기로 구분하여 입력하세요: ");
+//        String[] ary = new String[length];
+//        Scanner sc = new Scanner(System.in);
+//        for (int i = 0; i < length; i++) {
+//        ary[i] = sc.next();
+//        }
+//        System.out.println("====== 입력 완료 ======");
+//        System.out.println("====== 출력 시작 ======");
+//        for (int i = 0; i < length; i++) {
+//        System.out.println(ary[4 - i]);
