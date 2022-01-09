ee835194644c90d1640f8df5b8d4dd3cdfdbb6b9
john <john3210of@gmail.com>
2022-01-10 08:52:46 +0900

    mogakco_end

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,24 +1,35 @@
 import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 
-import java.util.Scanner;
-
-
-public class main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int[] value = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        while (true) {
-            System.out.print("숫자를 입력하세요: ");
-            int i = sc.nextInt();
-            System.out.println(value[i]);
-            System.out.println("프로그램 종료");
-            break;
+// Thread 클래스를 상속한 NumberThread 클래스 생성
+class NumberThread extends Thread {
+    public void run() { // start 메소드로 호출
+        // 0 부터 49까지 출력
+        for (int i = 0; i < 50; i++) {
+            System.out.print(i);
+        }
+    }
+}
+// Thread 클래스를 상속한 CharThread 클래스 생성
+class CharThread extends Thread {
+    public void run() { // start 메소드로 호출
+        // a부터 z까지 출력
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i);
         }
     }
 }
 
+public class main {
+    public static void main(String[] args) {
+        // NumberThread 클래스 인스턴스 생성
+        Thread thread1 = new NumberThread();
+        // CharThread 클래스 인스턴스 생성
+        Thread thread2 = new CharThread();
 
+        thread1.start(); // NumberThread 시작 (run 메소드 호출)
+        thread2.start(); // CharThread 시작 (run 메소드 호출)
+    }
+}
 //        System.out.println("hello 월드");
 //
 //        int a = 10;
